23e3a0185b51b6020d8b6d56efc4ac02c1dcbba6
unknown <yuhonguni@gmail.com>
2022-08-05 19:17:57 +0800

    1

--- GSE94769_methylation.R
+++ GSE94769_methylation.R
@@ -60,14 +60,27 @@ myload_met_gse <- champ.filter(beta=met_THYM_m,pd=meta_data_thymoma_kajiura,fixO
 ## normalization
 myNorm_met <- champ.norm(beta = myload_met_gse$beta,arraytype="450K", cores=8)
 
-#write.csv(myNorm_met,"tcga_met_normalization_update_MG_status_20220716.csv")
+write.csv(myNorm_met,"gse94769_met_normalization_update_MG_status_20220805.csv")
 
-myNorm_met<-read.csv("tcga_met_normalization_update_MG_status_20220716.csv",check.names = F)
+# myNorm_met<-read.csv("tcga_met_normalization_update_MG_status_20220716.csv",check.names = F)
 row.names(myNorm_met)<-myNorm_met$gene_id
 myNorm_met<-myNorm_met[,-1]
+
+myDMP <- champ.DMP(beta = myNorm_met,pheno=meta_data_thymoma_kajiura$Myasthenia_gravis,adjPVal = 1)
 
 head(myload_met$beta)
 head(myNorm_met)
 
+
+myDMP$no_to_yes %>% filter(gene == "TTN")
+myDMP$no_to_yes %>% filter(gene == "CHRNA1")
+myDMP$no_to_yes %>% filter(gene == "NEFM")
+myDMP$no_to_yes %>% filter(gene == "RYR1")
+myDMP$no_to_yes %>% filter(gene == "RYR2")
+myDMP$no_to_yes %>% filter(gene == "RYR3")
+myDMP$no_to_yes %>% filter(gene == "CHRND")
+myDMP$no_to_yes %>% filter(gene == "CHRNG")
+myDMP$no_to_yes %>% filter(gene == "CHRNE")
+myDMP$no_to_yes %>% filter(gene == "CHRNB1")
 #DMPs 分析
 myDMP <- champ.DMP(beta = myNorm_met,pheno=met_meta$MG,adjPVal = 1)
